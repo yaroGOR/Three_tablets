56cab7833066d351b0021c18031c8d3a2fa7a051
Yarik <36842297+yaroGOR@users.noreply.github.com>
2023-03-30 13:59:29 +0300

    camera fix

--- src/CameraController.jsx
+++ src/CameraController.jsx
@@ -3,10 +3,19 @@ import {  useThree } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export const CameraController = () => {
-   const { camera, gl } = useThree();
+   const {  gl } = useThree();
+   const camera = useThree(state => state.camera)
+
    useEffect(
       () => {
          const controls = new OrbitControls(camera, gl.domElement);
+         
+         controls.enableZoom=true
+         controls.enablePan=true
+         controls.enableRotate=true
+         controls.zoomSpeed=0.6
+         controls.panSpeed=0.5
+         controls.rotateSpeed=0.4
          controls.minDistance = 40;
          controls.maxDistance = 50;
          return () => {
